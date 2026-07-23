5e98f9de87aca65a556bb2a537110643c54c6b48
Guillermo <159314465+gillermillones@users.noreply.github.com>
2026-07-23 13:54:23 +0200

    overflowing tables

--- app/ui/items/show-values.tsx
+++ app/ui/items/show-values.tsx
@@ -30,32 +30,11 @@ export default function ShowValues({ item }:{ item: ItemData }) {
                 <th scope="col" className="px-3 py-5 font-small">
                     reusable
                 </th>
-                <th scope="col" className="px-3 py-5 font-small">
-                    portable
-                </th>
-                <th scope="col" className="px-3 py-5 font-small">
-                    toughness
-                </th>
-                <th scope="col" className="px-3 py-5 font-small">
-                    structure
-                </th>
-                <th scope="col" className="px-3 py-5 font-small">
-                    navigation
-                </th>
-                <th scope="col" className="px-3 py-5 font-small">
-                    operable
-                </th>
-                <th scope="col" className="px-3 py-5 font-small">
-                    av_accessible
-                </th>
-                <th scope="col" className="px-3 py-5 font-small">
-                    text_accessible
-                </th>
             </tr>
         </thead>
         <tbody className="bg-gray-200">
             <tr
-                key={item.id}
+                key={`${item.id}-1`}
                 className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
             >
                 <td className="whitespace-nowrap py-3 pl-6 pr-3">
@@ -82,6 +61,38 @@ export default function ShowValues({ item }:{ item: ItemData }) {
                 <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     {item.reusable}
                 </td>
+            </tr>
+        </tbody>
+        <thead className="rounded-lg text-left text-sm font-normal">
+            <tr>
+                <th scope="col" className="px-3 py-5 font-small">
+                    portable
+                </th>
+                <th scope="col" className="px-3 py-5 font-small">
+                    toughness
+                </th>
+                <th scope="col" className="px-3 py-5 font-small">
+                    structure
+                </th>
+                <th scope="col" className="px-3 py-5 font-small">
+                    navigation
+                </th>
+                <th scope="col" className="px-3 py-5 font-small">
+                    operable
+                </th>
+                <th scope="col" className="px-3 py-5 font-small">
+                    av_accessible
+                </th>
+                <th scope="col" className="px-3 py-5 font-small">
+                    text_accessible
+                </th>
+            </tr>
+        </thead>
+        <tbody className="bg-gray-200">
+            <tr 
+                key={`${item.id}-2`}
+                className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+            >
                 <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     {item.portable}
                 </td>
