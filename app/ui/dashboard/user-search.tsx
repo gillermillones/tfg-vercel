13dad02bedb18efbb58594e0854df98a3d8a1ca3
Guillermo <159314465+gillermillones@users.noreply.github.com>
2026-07-24 12:56:21 +0200

    skeleton tweak

--- app/ui/dashboard/user-search.tsx
+++ app/ui/dashboard/user-search.tsx
@@ -16,7 +16,7 @@ export default async function Page() {
             <div className="flex flex-col">
                 <div className="flex flex-row justify-end">
                     <input id="username" name="username" type="text" placeholder="Search username"
-                        className="peer block w-full rounded-md border border-gray-200 py-2 ml-1 text-sm outline-2 placeholder:text-gray-500"
+                        className="peer block w-full rounded-md border border-gray-200 py-2 mr-1 text-sm outline-2 placeholder:text-gray-500"
                         aria-describedby="name-error"
                     />
                     <button type="submit" className="rounded-md border p-2 bg-blue-400 hover:bg-blue-500">
